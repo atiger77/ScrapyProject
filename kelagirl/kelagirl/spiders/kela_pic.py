7bde339ba63053129abd2e723a54d94239871e70
Atiger77 <atiger77@126.com>
2017-03-08 23:08:21 +0800

    #add item

--- kelagirl/kelagirl/spiders/kela_pic.py
+++ kelagirl/kelagirl/spiders/kela_pic.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+from kelagirl.items import KelagirlItem
+
 class KeLaGirlSpider(scrapy.Spider):
     name="kelagirl"
     start_urls = [
-        "http://www.kelagirl.com/",
+        "http://www.kelagirls.com/zhuanji!findForIndexMoreTag.action?tagId=0&page=1",
+        "http://www.kelagirls.com/zhuanji!findForIndexMoreTag.action?tagId=0&page=2",
+        "http://www.kelagirls.com/zhuanji!findForIndexMoreTag.action?tagId=0&page=3",
     ]
 
     def parse(self,response):
-        print response.xpath('//div[@class="content"]').extract()
+        print response.xpath('//div[@class="zhuanjimorewrap"]/div/@pid').extract()
