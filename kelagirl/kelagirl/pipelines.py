0795be75f6d17060c36bbd34c275fdbf039b376a
Atiger77 <atiger77@126.com>
2017-03-08 23:57:09 +0800

    #add pipelines modle

--- kelagirl/kelagirl/pipelines.py
+++ kelagirl/kelagirl/pipelines.py
@@ -6,6 +6,19 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class KelagirlPipeline(object):
-    def process_item(self, item, spider):
+import scrapy
+from scrapy.pipeline.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+
+class KelagirlPipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        for image_url in item['image_urls']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, info):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        item['image_paths'] = image_paths
         return item
