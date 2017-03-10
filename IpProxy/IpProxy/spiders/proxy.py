3d75e51d13e65c40416f37320524d1b7cc766387
atiger77 <atiger77@126.com>
2017-03-10 16:48:50 +0800

    #add some ipproxy website

--- IpProxy/IpProxy/spiders/proxy.py
+++ IpProxy/IpProxy/spiders/proxy.py
@@ -1,6 +1,16 @@
 # -*- coding: utf-8 -*-
 
 import scrapy
+
+'''
+相关网站整理
+http://www.kuaidaili.com/free/
+http://www.xicidaili.com/
+http://www.proxy360.cn/default.aspx
+http://ip.zdaye.com/ 端口号是图片需要OCR
+'''
+
+
 class IpProxySpider(scrapy.Spider):
     name="IpProxy"
     start_urls = [
