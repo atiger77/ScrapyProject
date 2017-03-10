cbab0e36fbd92f2dd4fec83ececc7af62cd514fd
atiger77 <atiger77@126.com>
2017-03-10 17:57:13 +0800

    #add three website

--- IpProxy/IpProxy/spiders/proxy.py
+++ IpProxy/IpProxy/spiders/proxy.py
@@ -8,6 +8,9 @@ http://www.kuaidaili.com/free/
 http://www.xicidaili.com/
 http://www.proxy360.cn/default.aspx
 http://ip.zdaye.com/ 端口号是图片需要OCR
+http://www.cz88.net/proxy
+http://cn-proxy.com
+http://www.66ip.cn
 '''
 
 
