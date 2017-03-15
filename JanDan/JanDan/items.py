de6f88ef891d63189320ee37ea6f03343d6c2df2
Atiger77 <atiger77@126.com>
2017-03-15 23:42:36 +0800

    #add crawl spider of jiandan

--- JanDan/JanDan/items.py
+++ JanDan/JanDan/items.py
@@ -11,4 +11,5 @@ import scrapy
 class JandanItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    image_urls = scrapy.Field()
+    images = scrapy.Field()
--- JanDan/JanDan/spiders/jiandan_ooxx.py
+++ JanDan/JanDan/spiders/jiandan_ooxx.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
-import scrapy
-from rosioo.items import RosiooItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from JanDan.items import JandanItem
 
-class RosiokSpider(scrapy.Spider):
-    name = "rosiok"
-    allowed_domains = ["rosiok.com"]
-    start_urls = (
-        'http://jandan.net/ooxx/',
+class JandanSpider(CrawlSpider):
+    name = "JanDan"
+    allowed_domains = ["jandan.net"]
+    start_urls = [
+        "http://jandan.net/ooxx"
+    ]
+
+
+    rules = (
+        Rule(LinkExtractor(allow=('http://jandan.net/ooxx/page-\d+#comments', )), callback='parse_item', follow=True),
     )
 
+
+    def parse_item(self, response):
+        for href in response.xpath('//a[@class="view_img_link"]/@href').extract():
+            item = JandanItem(image_urls=[href])
+            yield item
