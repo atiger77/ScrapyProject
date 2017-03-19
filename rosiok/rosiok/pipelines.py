39f2571408778efc96306247da5a715e71a7d773
Atiger77 <atiger77@126.com>
2017-03-19 23:27:53 +0800

    #add code

--- rosiok/rosiok/pipelines.py
+++ rosiok/rosiok/pipelines.py
@@ -5,7 +5,20 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import scrapy
+from scrapy.exceptions import DropItem
+from scrapy.pipelines.images import ImagesPipeline
 
-class RosiokPipeline(object):
-    def process_item(self, item, spider):
-        return item
+class RosiooPipeline(ImagesPipeline):
+    def get_media_requests(self, item, spider):
+        for image_url in item['img_url']:
+            yield scrapy.Request(image_url)
+
+    def item_completed(self, results, item, spider):
+        image_paths = [x['path'] for ok, x in results if ok]
+        if not image_paths:
+            raise DropItem("Item contains no images")
+        item['image_paths'] = image_paths
+        return results
+
+
